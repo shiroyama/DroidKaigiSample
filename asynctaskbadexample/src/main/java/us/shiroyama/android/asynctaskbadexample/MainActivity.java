0a401ed822f2e20b97faca92ce729f4f102ae3e3
Fumihiko Shiroyama <fu.shiroyama@gmail.com>
2015-04-27 15:34:51 +0900

    First commitment

---
+++ asynctaskbadexample/src/main/java/us/shiroyama/android/asynctaskbadexample/MainActivity.java
@@ -0,0 +1,50 @@
+package us.shiroyama.android.asynctaskbadexample;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private TextView textView;
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.text);
+        button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new MyTask().execute();
+            }
+        });
+    }
+
+    private class MyTask extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                Log.e(TAG, "InterruptedException", e);
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            textView.setText("task finished");
+        }
+    }
+}
